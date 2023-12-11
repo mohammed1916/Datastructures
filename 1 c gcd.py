e0b64b89a4e69806d18d5f07d5f3bfbf4712e1e7
mohammed1916 <arrahmaan9901@gmail.com>
2023-12-11 23:09:04 +0530

    typo

--- 1 c gcd.py
+++ 1 c gcd.py
@@ -3,18 +3,17 @@ def gcdIterative(a, b):
         return abs(a)
     else:
         return gcdIterative(b, a % b)
- 
+# gcdIterative = lambda a, b: abs(a) if b == 0 else gcdIterative(b, a % b) 
 a = 60
 b = 48
  
 # prints 12
-print("The gcd of 60 and 48 calculated iteratively is : ", end="")
+print("The gcd of 60 and 48 calculated recursively is : ", end="")
 print(gcdIterative(60, 48))
 
 
-
-def gcdRecursive(x, y):
- 
+def gcdIterative(x, y):
+    # return max([i for i in range(1, min(x, y) + 1) if x % i == 0 and y % i == 0])
     if x > y:
         small = y
     else:
@@ -29,5 +28,5 @@ a = 60
 b = 48
  
 # prints 12
-print ("The gcd of 60 and 48 calculated recursively is : ", end="")
-print (gcdRecursive(60,48))
+print ("The gcd of 60 and 48 calculated iteratively is : ", end="")
+print (gcdIterative(60,48))
