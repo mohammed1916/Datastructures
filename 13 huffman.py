3559cf17c3fd43227e7bac7750b7034b1e8aa94c
mohammed1916 <arrahmaan9901@gmail.com>
2023-12-11 22:07:51 +0530

    comments in huffman code

--- 13 huffman.py
+++ 13 huffman.py
@@ -21,27 +21,27 @@ c_list = [('a', 5), ('b', 9), ('c', 12), ('d', 13), ('e', 16), ('f', 45)]
 c_sum = []
 def huffman(c_list):
     heap = []
-    for i in c_list:
-        heapq.heappush(heap, node(i[1], i[0]))
+    for node in c_list:
+        heapq.heappush(heap, node(node[1], node[0]))
     while len(heap) > 1:
         left = heapq.heappop(heap)
         right = heapq.heappop(heap)
         left.huff = 0
         right.huff = 1
-        new_node = node(left.freq + right.freq, left.symbol + right.symbol, left, right)
-        print(new_node.symbol)
-        heapq.heappush(heap, new_node)
+        parent = node(left.freq + right.freq, left.symbol + right.symbol, left, right)
+        print(f'{parent.symbol}->{parent.freq}')
+        heapq.heappush(heap, parent)
     return heap
 heap = huffman(c_list)
 def printNodes(node, val=''):
-    newVal = val + str(node.huff)
+    appendedCode = val + str(node.huff)
     if(node.left):
-        printNodes(node.left, newVal)
+        printNodes(node.left, appendedCode)
     if(node.right):
-        printNodes(node.right, newVal)
+        printNodes(node.right, appendedCode)
     if(not node.left and not node.right):
-        print(f"{node.symbol} -> {newVal}")
-        c_sum.append(len(newVal)*node.freq)
+        print(f"{node.symbol} -> {appendedCode}")
+        c_sum.append(len(appendedCode)*node.freq)
 printNodes(heap[0])
 print("Length is ",sum(c_sum))
 print("Average length is ",sum(c_sum)/len(c_list))
